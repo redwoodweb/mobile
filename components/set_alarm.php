63fbac4aff74ee19fa7521f3f84bf2cbf239d25e
rihno1 <redwoodweb@naver.com>
2020-08-31 01:03:14 +0900

    로보알람설정관리
    로보알람설정관리

--- components/set_alarm.php
+++ components/set_alarm.php
@@ -1,26 +1,117 @@
 
-<div class="login_wrapper">
+<div class="set_alarm_wrapper">
     <div class="inner">
         <div class="title">
-            <h1>로그인</h1>
-            <p>
-            회원은 로그인 해주시고, 비회원은 회원가입을 해주십시오.<br>
-            지차체 담당자분은 운영자분에게 요청하시기 바랍니다.
-            </p>
+            <h1>로보 알람 설정 관리</h1>
         </div>
-        <form class="login_form">
-            <div class="login_container">
-                <div class="input_grp">
-                    <input type="text" placeholder="아이디">
-                    <input type="text" placeholder="비밀번호">
-                </div>
-                <div class="btn btn_login">로그인</div>
+        <div id="user">
+          <div class="user_img">
+            <img src="./public/img/user_img@3x.png" alt="">
+          </div>
+          <div class="user_info">
+            <h3>로보 사용자 정보</h3>
+            <div class="inner">
+              <div class="name">김보미(여 1994.8.11)</div>
+              <div class="phone_number">010-1234-5678</div>
             </div>
-        </form>
-        <div class="btn_container">
-            <a href="#" class="link">회원가입</a>
-            <a href="#" class="link">아이디 찾기</a>
-            <a href="#" class="link">비밀번호 찾기</a>
+          </div>
+        </div>
+        <div class="edit-bar">
+          <label class="edit" for="edit">편집</label>
+          <button class="add"></button>
+        </div>
+        <div class="set_alarm">
+          <input type="checkbox" id="edit"/>
+          <div class="items">
+            <div class="alarm_set_info">
+              <label class="delete_btn">
+                <input type="checkbox"/>
+              </label>
+              <div class="time">오전 07:00</div>
+              <div class="subtext">아침 기상 알람</div>
+              <div class="detail_info">
+                <span>2시간 10분 후 알림</span><div class="devied_line"></div><span>2시간 10분 후 알림</span>
+              </div>
+            </div>
+            <div class="alarm_set_button">
+              <label class="switch">
+                <input type="checkbox" checked>
+                <span class="slider round"></span>
+              </label>
+            </div>
+          </div>
+          <div class="items">
+            <div class="alarm_set_info">
+              <label class="delete_btn">
+                <input type="checkbox"/>
+              </label>
+              <div class="time">오전 07:00</div>
+              <div class="subtext">아침 기상 알람</div>
+              <div class="detail_info">
+                <span>2시간 10분 후 알림</span><div class="devied_line"></div><span>2시간 10분 후 알림</span>
+              </div>
+            </div>
+            <div class="alarm_set_button">
+              <label class="switch">
+                <input type="checkbox" checked>
+                <span class="slider round"></span>
+              </label>
+            </div>
+          </div>
+          <div class="items">
+            <div class="alarm_set_info">
+              <label class="delete_btn">
+                <input type="checkbox">
+              </label>
+              <div class="time">오전 07:00</div>
+              <div class="subtext">아침 기상 알람</div>
+              <div class="detail_info">
+                <span>2시간 10분 후 알림</span><div class="devied_line"></div><span>2시간 10분 후 알림</span>
+              </div>
+            </div>
+            <div class="alarm_set_button">
+              <label class="switch">
+                <input type="checkbox" checked>
+                <span class="slider round"></span>
+              </label>
+            </div>
+          </div>
+          <div class="items">
+            <div class="alarm_set_info">
+              <label class="delete_btn">
+                <input type="checkbox">
+              </label>
+              <div class="time">오전 07:00</div>
+              <div class="subtext">아침 기상 알람</div>
+              <div class="detail_info">
+                <span>2시간 10분 후 알림</span><div class="devied_line"></div><span>2시간 10분 후 알림</span>
+              </div>
+            </div>
+            <div class="alarm_set_button">
+              <label class="switch">
+                <input type="checkbox" checked>
+                <span class="slider round"></span>
+              </label>
+            </div>
+          </div>
+          <div class="items">
+            <div class="alarm_set_info">
+              <label class="delete_btn">
+                <input type="checkbox">
+              </label>
+              <div class="time">오전 07:00</div>
+              <div class="subtext">아침 기상 알람</div>
+              <div class="detail_info">
+                <span>2시간 10분 후 알림</span><div class="devied_line"></div><span>2시간 10분 후 알림</span>
+              </div>
+            </div>
+            <div class="alarm_set_button">
+              <label class="switch">
+                <input type="checkbox" checked>
+                <span class="slider round"></span>
+              </label>
+            </div>
+          </div>
         </div>
     </div>
 </div>
--- no_header_layout.php
+++ no_header_layout.php
@@ -2,20 +2,13 @@
 $layout = 'mobile common';
 include 'header.php';
 ?>
-    <!-- 네비게이션 없을 경우 헤더 -->
-    <header>
-        <div class="logo">
-            <a href="/" alt="홈으로 가기"><img src="./public/img/top_logo.png" alt="GST 로고 이미지"></a>
-        </div>
-    </header>
-    <!-- //.네비게이션 없을 경우 헤더 -->
     <!-- content : 콘텐츠 영역 -->
     <div class="content">
         <!-- content_inner -->
         <div class="content_inner">
             <?php
                 //해당 컴포넌트 :
-                include 'components/login.php';//로그인
+                include 'components/set_alarm.php';//로보 알람설정 관리
             ?>
         </div>
         <!-- //.content_inner -->
